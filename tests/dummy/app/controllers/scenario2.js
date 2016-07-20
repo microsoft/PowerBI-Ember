97b1f062829d0fb4713d719f9dc94a3f2b6b14db
Matt Mazzola <mattm@microsoft.com>
2016-07-20 13:23:39 -0700

    Update application to match other sample app presentation.

--- tests/dummy/app/controllers/scenario2.js
+++ tests/dummy/app/controllers/scenario2.js
@@ -32,8 +32,16 @@ export default Ember.Controller.extend({
     return fetch(`${this.apiBaseUrl}/api/reports/${reportId}`)
       .then(response => response.json())
       .then(report => {
-        report.type = 'report';
-        report.filterPaneEnabled = this.get('filterPaneEnabled');
+        Ember.$.extend(report, {
+          type: 'report',
+          embedUrl: 'https://portal.analysis.windows-int.net/appTokenReportEmbed',
+          id: 'c4d31ef0-7b34-4d80-9bcb-5974d1405572',
+          accessToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2ZXIiOiIwLjEuMCIsImF1ZCI6Imh0dHBzOi8vYW5hbHlzaXMud2luZG93cy5uZXQvcG93ZXJiaS9hcGkiLCJpc3MiOiJQb3dlckJJU0RLIiwidHlwZSI6ImVtYmVkIiwid2NuIjoiV2FsbGFjZSIsIndpZCI6IjUyMWNkYTJhLTRlZDItNDg5Ni1hYzA0LWM5YzM4MWRjMjUyYSIsInJpZCI6ImM0ZDMxZWYwLTdiMzQtNGQ4MC05YmNiLTU5NzRkMTQwNTU3MiIsIm5iZiI6MTQ2OTAzOTM0NSwiZXhwIjoxNDY5MDQyOTQ1fQ.vMFKofPrASKsiULThLkUqNhURFWobQ6pHIkbIM3kOng',
+          settings: {
+            filterPaneEnabled: this.get('filterPaneEnabled')
+          }
+        });
+
         this.set('report', report);
       });
   },
