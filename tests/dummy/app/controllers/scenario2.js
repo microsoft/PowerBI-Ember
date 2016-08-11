84c3b9add36ea7500047faa260846fdd72eaa5bd
Matt Mazzola <mattm@microsoft.com>
2016-08-11 13:14:19 -0700

    Remove hardcoding of embed urls and access tokens, replace with prod / dxt.

--- tests/dummy/app/controllers/scenario2.js
+++ tests/dummy/app/controllers/scenario2.js
@@ -6,7 +6,7 @@ const {
 } = Ember;
 
 export default Ember.Controller.extend({
-  apiBaseUrl: 'http://powerbipaasapi.azurewebsites.net',
+  apiBaseUrl: 'https://powerbiembedapi.azurewebsites.net',
   filter: null,
   filterPaneEnabled: false,
   reports: null,
@@ -33,10 +33,6 @@ export default Ember.Controller.extend({
       .then(response => response.json())
       .then(report => {
         Ember.$.extend(report, {
-          type: 'report',
-          embedUrl: 'https://portal.analysis.windows-int.net/appTokenReportEmbed',
-          id: 'c4d31ef0-7b34-4d80-9bcb-5974d1405572',
-          accessToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2ZXIiOiIwLjEuMCIsImF1ZCI6Imh0dHBzOi8vYW5hbHlzaXMud2luZG93cy5uZXQvcG93ZXJiaS9hcGkiLCJpc3MiOiJQb3dlckJJU0RLIiwidHlwZSI6ImVtYmVkIiwid2NuIjoiV2FsbGFjZSIsIndpZCI6IjUyMWNkYTJhLTRlZDItNDg5Ni1hYzA0LWM5YzM4MWRjMjUyYSIsInJpZCI6ImM0ZDMxZWYwLTdiMzQtNGQ4MC05YmNiLTU5NzRkMTQwNTU3MiIsIm5iZiI6MTQ3MDMzMDM4NywiZXhwIjoxNDcwMzMzOTg3fQ.UtqfOTWYhaNqAWaF43X98VGjVk3vaV-Vl-MkQtypPJQ',
           settings: {
             filterPaneEnabled: this.get('filterPaneEnabled')
           }
--- tests/dummy/app/routes/scenario4.js
+++ tests/dummy/app/routes/scenario4.js
@@ -3,14 +3,10 @@ import fetch from 'ember-network/fetch';
 
 export default Ember.Route.extend({
   model() {
-    return fetch('http://powerbipaasapi.azurewebsites.net/api/reports/5dac7a4a-4452-46b3-99f6-a25915e0fe55')
+    return fetch('https://powerbiembedapi.azurewebsites.net/api/dxt/reports/c4d31ef0-7b34-4d80-9bcb-5974d1405572')
       .then(response => response.json())
       .then(embedConfiguration => {
         return Ember.$.extend(embedConfiguration, {
-          type: 'report',
-          embedUrl: 'https://portal.analysis.windows-int.net/appTokenReportEmbed?unmin=true',
-          id: 'c4d31ef0-7b34-4d80-9bcb-5974d1405572',
-          accessToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2ZXIiOiIwLjEuMCIsImF1ZCI6Imh0dHBzOi8vYW5hbHlzaXMud2luZG93cy5uZXQvcG93ZXJiaS9hcGkiLCJpc3MiOiJQb3dlckJJU0RLIiwidHlwZSI6ImVtYmVkIiwid2NuIjoiV2FsbGFjZSIsIndpZCI6IjUyMWNkYTJhLTRlZDItNDg5Ni1hYzA0LWM5YzM4MWRjMjUyYSIsInJpZCI6ImM0ZDMxZWYwLTdiMzQtNGQ4MC05YmNiLTU5NzRkMTQwNTU3MiIsIm5iZiI6MTQ3MDMzMDM4NywiZXhwIjoxNDcwMzMzOTg3fQ.UtqfOTWYhaNqAWaF43X98VGjVk3vaV-Vl-MkQtypPJQ',
           settings: {
             filterPaneEnabled: false
           }
