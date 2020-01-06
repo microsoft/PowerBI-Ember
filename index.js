3cf9edb93f827e43a0bb5487625b77a4960a980f
Ed Huang <ehuang@soxhub.com>
2020-01-06 14:57:44 -0800

    updated to vendor

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-powerbi',
+	name: 'ember-powerbi',
 
-  isDevelopingAddon: function() {
-    return true;
-  },
+	isDevelopingAddon: function() {
+		return true;
+	},
 
-  included: function(app) {
-    this._super.included.apply(this, arguments);
-    
-    app.import(app.bowerDirectory + '/powerbi-client/dist/powerbi.js');
-    app.import('vendor/shims/powerbi.js');
-    app.import('vendor/shims/powerbi-client.js');
-  }
+	included: function(app) {
+		this._super.included.apply(this, arguments);
+
+		app.import('vendor/powerbi-client/dist/powerbi.js');
+		app.import('vendor/shims/powerbi.js');
+		app.import('vendor/shims/powerbi-client.js');
+	}
 };
